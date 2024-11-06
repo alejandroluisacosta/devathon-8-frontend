ccd6caaf4597afca5cd3fad8922c0cc7b513d90c
Joselu <joseluissgz97@gmail.com>
2024-11-06 15:23:56 +0100

    feat: add map preview

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
+import mapboxgl from 'mapbox-gl';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import './index.scss';
+
+const API_KEY = import.meta.env.VITE_MAP_BOX_TOKEN;
+
+mapboxgl.accessToken = API_KEY;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
