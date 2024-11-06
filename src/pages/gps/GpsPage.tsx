8e882e2bc1bcac92af7f70f937ab105cda96f731
Jose Luis <89526283+JoseIu@users.noreply.github.com>
2024-11-06 15:31:21 +0100

    feat: add map preview (#6)

--- src/pages/gps/GpsPage.tsx
+++ src/pages/gps/GpsPage.tsx
@@ -1,9 +1,26 @@
+import { Map } from 'mapbox-gl';
+import { useLayoutEffect, useRef } from 'react';
 import './gps.scss';
 
 export const GpsPage = () => {
+  const mapDiv = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<Map | null>(null);
+
+  useLayoutEffect(() => {
+    if (mapRef.current || !mapDiv.current) return;
+
+    mapRef.current = new Map({
+      container: mapDiv.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40],
+      zoom: 9,
+    });
+  }, []);
   return (
     <div className="gps">
-      <div>GpsPage</div>
+      <div className="gps__container">
+        <div ref={mapDiv} className="map"></div>
+      </div>
     </div>
   );
 };
