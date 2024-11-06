edaec9557bdee46b2b968b2f6be45a305580fd25
Alejandro <alejandroluisacosta95@gmail.com>
2024-11-06 17:18:43 +0100

    add: input field styles - gps search component

--- src/pages/gps/GpsPage.tsx
+++ src/pages/gps/GpsPage.tsx
@@ -22,9 +22,11 @@ export const GpsPage = () => {
     <div className="gps">
       <div className="gps__container">
         <div ref={mapDiv} className="map"></div>
-        <input type='text' className='gps__input gps__input--origin'/>
-        <input type='text' className='gps__input gps__input--destination'/>
-        <button type='submit' className='gps__submit'>Start</button>
+        <div className='gps__search-container'>
+          <input type='text' className='gps__input gps__input--origin'/>
+          <input type='text' className='gps__input gps__input--destination'/>
+          <button type='submit' className='gps__submit'>Start</button>
+        </div>
       </div>
     </div>
   );
