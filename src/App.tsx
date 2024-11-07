e7f831c1b6ffb0c309403e854c4cfb04036124ac
Alejandro Luis Acosta <98220394+alejandroluisacosta@users.noreply.github.com>
2024-11-07 17:24:15 +0100

    Feature: GPS search bar (#8)
    * add: input elements for search bar
    
    * add: input field styles - gps search component
    
    * add: react context for gps "destination" prop
    
    * add: wrap gps component within destination provider
    
    * fix: wrap only gps element on DestinationContext to fix console error
    
    * add: request origin and destination coordinates from mapbox api
    
    * add: map centers on new origin location for the trip
    
    * add: fetch route from mapbox api
    
    * add: function to add route on top of map
    
    * add: function to retrieve location suggestions as the user types
    
    * add: debounce on api requests for suggestions
    
    * add: onChange handler for address suggestions on destination input element
    
    * fix: remove autocomplete function from input elements
    
    * fix: fetch route only if coordinates for both origin and destination have been provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { CaloriesPage, ChillPage, ElvesPage, GpsPage, GradesPage, ReaderPage, ReindeersPage } from './pages';
 import { DashboardTemplate } from './template/DashboardTemplate';
+import { DestinationProvider } from './context/DestinationContext';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<DashboardTemplate />}>
-        <Route index element={<GpsPage />} />
+          <Route index element={<DestinationProvider><GpsPage /></DestinationProvider>} />
         <Route path="reindeers" element={<ReindeersPage />} />
         <Route path="grades" element={<GradesPage />} />
         <Route path="calories" element={<CaloriesPage />} />
