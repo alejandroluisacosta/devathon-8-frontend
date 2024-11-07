deffb4f0fc49e4979b2962ef81c4cda2a037850a
Alejandro <alejandroluisacosta95@gmail.com>
2024-11-07 10:39:59 +0100

    add: request origin and destination coordinates from mapbox api

--- src/pages/gps/GpsPage.tsx
+++ src/pages/gps/GpsPage.tsx
@@ -1,6 +1,7 @@
 import { Map } from 'mapbox-gl';
 import { useLayoutEffect, useRef } from 'react';
 import './gps.scss';
+import { API_KEY } from '../../main';
 
 export const GpsPage = () => {
   const mapDiv = useRef<HTMLDivElement>(null);
@@ -17,13 +18,25 @@ export const GpsPage = () => {
     });
   }, []);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const { origin, destination } = event.target as typeof event.target & {
       origin: { value: string };
       destination: { value: string };
     }
-    console.log(origin, destination);
+
+    // Request origin coordinates
+    const originResponse = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${origin.value}.json?access_token=${API_KEY}`);
+    const originData = await originResponse.json();
+    const originCoordinates = originData.features[0].center;
+    console.log(originCoordinates);
+
+    // Request destination coordinates
+    const destinationResponse = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${destination.value}.json?access_token=${API_KEY}`);
+    const destinationData = await destinationResponse.json();
+    const destinationCoordinates = destinationData.features[0].center;
+    console.log(destinationCoordinates);
+
   }
   
   return (
