45b5306aa7ece881bc6efffe4adc9d4f94187a8f
Alejandro Luis Acosta <98220394+alejandroluisacosta@users.noreply.github.com>
2024-11-08 12:09:05 +0100

    fix: multiple route searches are now possible without reloading the page (#9)

--- src/utils/mapUtils.ts
+++ src/utils/mapUtils.ts
@@ -3,6 +3,12 @@ import { Map } from 'mapbox-gl';
 export const addRouteToMap = (mapRef: React.RefObject<Map>, routeGeoJson: any) => {
   if (!mapRef.current || !routeGeoJson) return;
 
+  // Remove previous route if existing
+  if (mapRef.current.getSource('route')) {
+    mapRef.current.removeLayer('route');
+    mapRef.current.removeSource('route');
+  }
+
   // Add the route data as a source
   mapRef.current.addSource('route', {
     type: 'geojson',
