569caf81aa468b9f65be7915c03513236e96b070
Miguel Decode <migueldecode@gmail.com>
2024-11-10 12:32:35 +0100

    feat: add page RegistryPage and export

---
+++ src/pages/registry/RegistryPage.tsx
@@ -0,0 +1,7 @@
+import RegistrySearches from '../../components/registrySearches/RegistrySearches';
+
+const RegistryPage = () => {
+  return <RegistrySearches />;
+};
+
+export default RegistryPage;
