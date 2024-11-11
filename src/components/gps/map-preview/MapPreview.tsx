02c43d29b9cbd157fcff37f392ef8cf79713c045
Joselu <joseluissgz97@gmail.com>
2024-11-11 10:40:37 +0100

    feat: add MappPreview and seting map for MapContext

---
+++ src/components/gps/map-preview/MapPreview.tsx
@@ -0,0 +1,28 @@
+import { Map } from 'mapbox-gl';
+import { useLayoutEffect, useRef } from 'react';
+import { useMap } from '../../../hook/useMap';
+import { usePlaces } from '../../../hook/usePlaces';
+import { MapSkeleton } from '../../ui/map-skeleton/MapSkeleton';
+
+export const MapPreview = () => {
+  const { isLoading, userLocation } = usePlaces();
+  const { setMap } = useMap();
+
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (isLoading) return;
+
+    const map = new Map({
+      container: mapDiv.current!,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: userLocation,
+      zoom: 12,
+    });
+    setMap(map);
+  }, [isLoading, userLocation]);
+
+  if (isLoading) return <MapSkeleton />;
+
+  return <div ref={mapDiv} className="map"></div>;
+};
