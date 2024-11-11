1b003a0dd63b33e3d34879d018cf3c877ac7ccde
Joselu <joseluissgz97@gmail.com>
2024-11-11 10:43:19 +0100

    feat: add mapSkeleton and searchresultSkeleton

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
+export * from './gps/grinch-marker/GrinchMarker';
+export * from './gps/map-preview/MapPreview';
+export * from './gps/search-bar/SearchBar';
+export * from './ui/map-skeleton/MapSkeleton';
 export * from './ui/sidebar/SideBar';
