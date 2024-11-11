d2c54a3964865fadb50d45f0018b9d687287d144
Miguel Decode <migueldecode@gmail.com>
2024-11-11 11:57:20 +0100

    fix: remove RegistryPage

--- src/pages/registry/RegistryPage.tsx
+++
@@ -1,7 +0,0 @@
-import RegistrySearches from '../../components/registrySearches/RegistrySearches';
-
-const RegistryPage = () => {
-  return <RegistrySearches />;
-};
-
-export default RegistryPage;
