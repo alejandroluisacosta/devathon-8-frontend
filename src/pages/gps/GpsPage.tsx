e0e1022e3e3c39d7bc8b4d6dc0a9caec5f91d967
Alejandro <alejandroluisacosta95@gmail.com>
2024-11-12 09:42:01 +0100

    add: gps subtitle

--- src/pages/gps/GpsPage.tsx
+++ src/pages/gps/GpsPage.tsx
@@ -5,6 +5,7 @@ export const GpsPage = () => {
   return (
     <div className="gps">
       <h1 className='gps__title'>Christmas Map</h1>
+      <p className='gps__subtitle'>'Cause getting lost on Christmas Eve wouldn't make Santa happy!</p>
       <SearchBar />
       <div className="gps__container">
         <MapPreview />
