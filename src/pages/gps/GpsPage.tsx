8a7fe164f4dc7b8cb7b83dc290712cf7d9700377
Alejandro <alejandroluisacosta95@gmail.com>
2024-11-12 09:23:03 +0100

    add: great vibes font family for titles

--- src/pages/gps/GpsPage.tsx
+++ src/pages/gps/GpsPage.tsx
@@ -4,6 +4,7 @@ import './gps.scss';
 export const GpsPage = () => {
   return (
     <div className="gps">
+      <h1 className='gps__title'>Christmas Map</h1>
       <SearchBar />
       <div className="gps__container">
         <MapPreview />
