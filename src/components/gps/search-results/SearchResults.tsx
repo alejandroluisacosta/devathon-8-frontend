3442068de1778a768a68864a69358402bc86d4bd
Joselu <joseluissgz97@gmail.com>
2024-11-15 15:52:11 +0100

    feat: save selected place to history in SearchResults component

--- src/components/gps/search-results/SearchResults.tsx
+++ src/components/gps/search-results/SearchResults.tsx
@@ -1,12 +1,15 @@
 import { Marker } from 'mapbox-gl';
 import { useState } from 'react';
 import { useMap, usePlaces } from '../../../hook';
+import { useHistory } from '../../../hook/useHistory';
+import { IntSearch } from '../../../interfaces/history.interface';
 import { Feature } from '../../../interfaces/PlacesResponce.interface';
 import { SearchResultsSkeleton } from '../../ui/search-results-skleton/SearchResultsSkeleton';
 import './searchResults.scss';
 export const SearchResults = () => {
   const { places, isLoadingPlaces, userLocation } = usePlaces();
   const { map, getRouteBetweenPoints } = useMap();
+  const { updateHistory } = useHistory();
   const [activeMarker, setActiveMarker] = useState<Marker | null>(null);
   const [activeId, setActiveId] = useState<string>('');
 
@@ -29,6 +32,19 @@ export const SearchResults = () => {
     if (!userLocation) return;
     const [lng, lat] = place.geometry.coordinates;
     getRouteBetweenPoints(userLocation, [lng, lat]);
+
+    //Save the place in the history
+    const historyToSave: IntSearch = {
+      type: 'addresses',
+      id: parseInt(place.id),
+      attributes: {
+        place: place.properties.name_preferred,
+        city: place.properties.full_address,
+        country: place.properties.context.country.name,
+        coordinates: [lng, lat],
+      },
+    };
+    updateHistory(historyToSave);
   };
 
   if (!places.length) return <></>;
@@ -50,7 +66,7 @@ export const SearchResults = () => {
               <button className="search-results__button" onClick={() => getRoute(place)}>
                 Directions
               </button>
-              <img src='images/bauble.png' className='search-results__bauble'/>
+              <img src="images/bauble.png" className="search-results__bauble" />
             </li>
           ))}
         </ul>
