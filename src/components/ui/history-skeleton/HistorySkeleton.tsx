5f21dec74f79209e09a0823829b7e8755064a16e
Miguel Decode <migueldecode@gmail.com>
2024-11-28 19:33:23 +0100

    feature: add skeleton history

---
+++ src/components/ui/history-skeleton/HistorySkeleton.tsx
@@ -0,0 +1,13 @@
+
+import './HistorySkeleton.scss';
+
+export const HistorySkeleton = () => {
+  return (
+    <div className="skeleton">
+      <div className="skeleton__history"></div>
+      <div className="skeleton__history"></div>
+      <div className="skeleton__history"></div>
+      <div className="skeleton__history"></div>
+    </div>
+  );
+};
