fbe6a351f9a54b48de37e94bd157dbb5fa4f0650
Miguel Decode <migueldecode@gmail.com>
2024-12-11 18:28:13 +0100

    feature: add new page ElveForm

---
+++ src/pages/elve-form/ElveFormPage.tsx
@@ -0,0 +1,10 @@
+import ElveForm from '../../components/elve-form/ElveForm';
+import './ElveFormPage.scss';
+
+export const ElveFormPage = () => {
+  return (
+    <div className="center">
+      <ElveForm />
+    </div>
+  );
+};
