fae71edc3618a04c6e42f30d4c9b0411e7b6cdf7
Miguel Decode <97762748+MiguelDecode@users.noreply.github.com>
2024-12-12 19:57:04 +0100

    Feature/btn new elve (#40)
    * build: add react-hook-form dependency
    
    * build: add zod dependency
    
    * feature: create a component form to update or add an elve
    
    * feature: add logic to add a new elve
    
    * fix: add max-width to ElveForm component
    
    * feature: add elve-form page route
    
    * fix: remove import Navigate from react-router-dom
    
    * feature: add link to add a new elve
    
    * feature: add export to barrel file of ElveFormPage
    
    * feature: add new page ElveForm

--- src/components/elves/elve-list/ElveList.tsx
+++ src/components/elves/elve-list/ElveList.tsx
@@ -1,6 +1,8 @@
+import { FaPlus } from 'react-icons/fa';
 import { Elve } from '../../../interfaces';
 import { ElveCarD } from '../elve-card/ElveCard';
 import './elvelList.scss';
+import { NavLink } from 'react-router-dom';
 
 type Props = {
   elvesList: Elve[];
@@ -12,6 +14,9 @@ export const ElveList = ({ elvesList }: Props) => {
       {elvesList.map((elve) => (
         <ElveCarD key={elve.id} elve={elve} />
       ))}
+      <NavLink className="nav-icon" to="/elve-form">
+        <FaPlus />
+      </NavLink>
     </div>
   );
 };
