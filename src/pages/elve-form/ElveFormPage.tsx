815c7d2769be52f0edca3fc35c81968a3f561e5b
Jose Luis <89526283+JoseIu@users.noreply.github.com>
2024-12-13 18:02:58 +0100

    feat: add edit elve (#44)

--- src/pages/elve-form/ElveFormPage.tsx
+++ src/pages/elve-form/ElveFormPage.tsx
@@ -1,10 +1,20 @@
+import { useParams } from 'react-router-dom';
 import ElveForm from '../../components/elve-form/ElveForm';
+import { useElves } from '../../hook';
 import './ElveFormPage.scss';
 
 export const ElveFormPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const { elves } = useElves();
+  let elveToEdit = undefined;
+
+  if (id) {
+    elveToEdit = elves.find((elve) => elve.id === +id);
+  }
+
   return (
     <div className="center">
-      <ElveForm />
+      <ElveForm elve={elveToEdit} />
     </div>
   );
 };
